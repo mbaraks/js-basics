dc98de840b7b0731ecdce4c999202121f7dc65fe
mbaraks <mbaraks603@gmail.com>
2025-07-22 10:53:54 +0300

    Asks for a name and outputs a greeting message.

--- script.js
+++ script.js
@@ -17,4 +17,41 @@ console.log(pi);
 
 console.log(5 + 10);
 
+console.log(5 + 10 + 15 + 20 + 25 + 30 + 35 + 40 + 45 + 50);
 
+//Subtraction of numbers
+console.log(100 - 57);
+
+//Multiplication of numbers
+console.log(5 * 10);
+
+//Division of numbers
+console.log(100 / 5);
+
+//Modulus of numbers
+console.log(100 % 3);
+
+//exponentiation of numbers
+console.log(2 ** 3);
+
+console.log ((4 + 6 + 9) / 77);
+
+let a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+let b = 7 * a;
+console.log(b);
+
+
+const max = 57;
+const actual = max - 13;
+const percentage = (actual / max);
+console.log(percentage);
+
+
+//alert("Not a number/30");
+
+prompt("Enter your name: ", "John Doe");
+confirm("Are you sure you want to proceed?");
